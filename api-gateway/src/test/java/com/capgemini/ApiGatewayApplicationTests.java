217fbf28db3a1a769e1a337f43436844bffc527a
cser-srishti-beaspal <cser.srishti.beaspal@gmail.com>
2026-03-28 15:19:24 +0530

    api-gateway

---
+++ api-gateway/src/test/java/com/capgemini/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
